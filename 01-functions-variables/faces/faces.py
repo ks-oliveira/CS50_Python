47289ddf70f244ae378ab645d7748dd7444ad838
Kelwyn Oliveira <77862988+kelwynOliveira@users.noreply.github.com>
2023-11-26 19:40:59 -0400

    Adding projects

--- 01-functions-variables/faces/faces.py
+++ 01-functions-variables/faces/faces.py
@@ -2,16 +2,10 @@ def main():
     text = input()
     print(convert(text))
 
-
 def convert(text):
-    faced_smile = ':)' in text
-    faced_sad = ':(' in text
-
-    if(faced_smile or faced_sad):
-        text = text.replace(':)', '🙂')
-        text = text.replace(':(', '🙁')
+    text = text.replace(':)', '🙂')
+    text = text.replace(':(', '🙁')
 
     return text
-
 
 main()
